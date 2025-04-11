c1b0fa91335e96cada88fbb79787fb0ca09439d9
Harsh Kedia <hkedia@hey.com>
2025-04-11 19:56:00 +0530

    using redux toolkit query

--- src/store/index.js
+++ src/store/index.js
@@ -3,13 +3,26 @@ import { usersReducer } from "./slices/usersSlice";
 import { fetchUsers } from "./thunks/fetchUsers";
 import { addUser } from "./thunks/addUser";
 import { removeUser } from "./thunks/removeUser";
+import { setupListeners } from "@reduxjs/toolkit/query";
+import { albumsApi } from "./apis/albumsApi";
+import { photosApi } from "./apis/photosApi";
 
 export const store = configureStore({
   reducer: {
-    users: usersReducer
+    users: usersReducer,
+    [albumsApi.reducerPath]: albumsApi.reducer,
+    [photosApi.reducerPath]: photosApi.reducer
+  },
+  middleware: (getDefaultMiddleware) => {
+    return getDefaultMiddleware()
+      .concat(albumsApi.middleware)
+      .concat(photosApi.middleware)
   }
 })
 
-export { fetchUsers, addUser, removeUser }
+setupListeners(store.dispatch)
 
+export { fetchUsers, addUser, removeUser }
+export { useFetchAlbumsQuery, useAddAlbumMutation, useRemoveAlbumMutation } from './apis/albumsApi'
+export { useFetchPhotosQuery, useAddPhotoMutation, useRemovePhotoMutation } from './apis/photosApi'
 
